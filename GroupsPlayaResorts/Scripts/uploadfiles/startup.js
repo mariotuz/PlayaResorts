55360e5af0dbec306ab959671f58933e1326fa77
Mario.Tuz <mariotuz1990@gmail.com>
2016-08-09 16:40:25 -0500

    d

--- GroupsPlayaResorts/Scripts/uploadfiles/startup.js
+++
@@ -1,9 +0,0 @@
- $(document).ready(function()
-      {
-        $("#multipleupload").uploadFile({
-        url:"http://dierection url/file.php",
-        multiple:true,
-        allowedTypes:"doc,docx,xlsx,xls,pdf",
-        fileName:"myfile"
-        });
-      });
